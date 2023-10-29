94c553a041c3d67edeb60cb26a971e11ea290f59
Yiyuan Zhong <824549303@qq.com>
2023-10-30 00:35:32 +0800

    feature: wavelet compression (not finished)

--- src/Forward_modelling/Fwd.cpp
+++ src/Forward_modelling/Fwd.cpp
@@ -1,31 +1,38 @@
 #include "Fwd.h"
 
-Fwd::Fwd() : field_flag(Compute_g_z) {
+Fwd::Fwd() : field_flag(Compute_g_z)
+{
   // this->mesh = NULL;
   // this->ob = NULL;
   Nm = 0;
   N_obs = 0;
   int n_fields = field_flag.count();
   Nd = N_obs * n_fields;
+  use_wavelet = false;
+  compression_threshold = 0;
 }
 
 Fwd::~Fwd() {}
 
-Fwd::Fwd(const Mesh& mesh_,
-         const Observation& ob_,
+Fwd::Fwd(const Mesh &mesh_,
+         const Observation &ob_,
          unsigned long long field_flag_)
-    : field_flag(field_flag_) {
+    : field_flag(field_flag_)
+{
   mesh = mesh_;
   ob = ob_;
   Nm = mesh.n_elems();
   N_obs = ob.get_n_obs();
   int n_fields = field_flag.count();
   Nd = N_obs * n_fields;
+  use_wavelet = false;
+  compression_threshold = 0;
   display_info_fields();
 }
 
-Fwd::Fwd(const Mesh& mesh_, const Observation& ob_, bitset<10> field_flag_)
-    : field_flag(field_flag_) {
+Fwd::Fwd(const Mesh &mesh_, const Observation &ob_, bitset<10> field_flag_)
+    : field_flag(field_flag_)
+{
   mesh = mesh_;
   ob = ob_;
 
@@ -33,10 +40,13 @@ Fwd::Fwd(const Mesh& mesh_, const Observation& ob_, bitset<10> field_flag_)
   N_obs = ob.get_n_obs();
   int n_fields = field_flag.count();
   Nd = N_obs * n_fields;
+  use_wavelet = false;
+  compression_threshold = 0;
   display_info_fields();
 }
 
-void Fwd::compute_G() {
+void Fwd::compute_G()
+{
   int n_fields = field_flag.count();
   assert(Nm > 0);
   assert(N_obs > 0);
@@ -44,45 +54,55 @@ void Fwd::compute_G() {
   G.resize(n_fields * N_obs, Nm);
 
   vector<int> basic_field_index;
-  for (int i = 0; i < 10; i++) {
+  for (int i = 0; i < 10; i++)
+  {
     // bool status = flag[i];
-    if (field_flag[i]) {
+    if (field_flag[i])
+    {
       basic_field_index.push_back(i);
     }
   }
 
-  for (int i = 0; i < N_obs; i++) {
+  for (int i = 0; i < N_obs; i++)
+  {
 #pragma omp parallel for
-    for (int j = 0; j < Nm; j++) {
+    for (int j = 0; j < Nm; j++)
+    {
       GravFormula gra;
       vector<double> field;
       gra.field_caused_by_single_prism(ob(i), mesh.get_elem(j), 1.0, field,
-                                field_flag);
-      for (int k = 0; k < n_fields; k++) {
+                                       field_flag);
+      for (int k = 0; k < n_fields; k++)
+      {
         G(i + k * N_obs, j) = field[basic_field_index[k]];
       }
     }
   }
 }
 
-void Fwd::set_mesh(const Mesh& mesh0) {
+void Fwd::set_mesh(const Mesh &mesh0)
+{
   this->mesh = mesh0;
   Nm = mesh.n_elems();
 }
 
-void Fwd::set_observation(const Observation& ob0) {
+void Fwd::set_observation(const Observation &ob0)
+{
   this->ob = ob0;
   N_obs = ob.get_n_obs();
 }
 
-void Fwd::display_info_fields() const {
+void Fwd::display_info_fields() const
+{
   int n = field_flag.count();
-  vector<string> strs = {"V",          "g_z",      "g_x", "g_y",
-                         "T_zz",       "T_xz", "T_yz",  "T_xx",
+  vector<string> strs = {"V", "g_z", "g_x", "g_y",
+                         "T_zz", "T_xz", "T_yz", "T_xx",
                          "T_xy", "T_yy"};
   vector<int> to_be_computed;
-  for (int i = 0; i < strs.size(); i++) {
-    if (field_flag[i]) {
+  for (int i = 0; i < strs.size(); i++)
+  {
+    if (field_flag[i])
+    {
       to_be_computed.push_back(i);
     }
   }
@@ -91,8 +111,175 @@ void Fwd::display_info_fields() const {
        << (n > 1 ? " gravity field components are"
                  : " gravity field component is")
        << " used. " << (n > 1 ? "They are: \t" : "It is: \t");
-  for (int i = 0; i < n; i++) {
+  for (int i = 0; i < n; i++)
+  {
     cout << strs[to_be_computed[i]] << ((n > 1 && i == n - 2) ? " and " : ", ");
   }
   cout << endl;
+}
+
+int Fwd::inverse_wavelet_transform_vec(vector<double> &data) const
+{
+  int n = data.size();
+  int n_dy;
+
+  double log_value = std::log2(1.0 * n);
+  if (std::fabs(log_value - int(log_value)) < 1e-15)
+  {
+    n_dy = n;
+  }
+  else
+  {
+    n_dy = std::pow(2, ceil(log2(1.0 * n)));
+    vector<double> data_copy = data;
+    data.resize(n_dy);
+#pragma omp parallel for
+    for (int i = 0; i < n; i++)
+    {
+      data[i] = data_copy[i];
+    }
+    // padding
+#pragma omp parallel for
+    for (int i = n; i < n_dy; i++)
+    {
+      data[i] = 0;
+    }
+  }
+
+  gsl_wavelet *w;
+  gsl_wavelet_workspace *work;
+  w = gsl_wavelet_alloc(gsl_wavelet_daubechies, 4);
+  work = gsl_wavelet_workspace_alloc(n_dy);
+
+  double *p_data = data.data();
+  gsl_wavelet_transform_inverse(w, p_data, 1, n_dy, work);
+  gsl_wavelet_free(w);
+  gsl_wavelet_workspace_free(work);
+  return n_dy;
+}
+
+int Fwd::compress_vec(vector<double> &data, vector<int> &ids,
+                      vector<double> &coeffs, double relative_threshold)
+{
+  int n = data.size();
+  int n_dy;
+
+  double log_value = std::log2(1.0 * n);
+  if (std::fabs(log_value - int(log_value)) < 1e-15)
+  {
+    n_dy = n;
+  }
+  else
+  {
+    n_dy = std::pow(2, ceil(log2(1.0 * n)));
+    vector<double> data_copy = data;
+    data.resize(n_dy);
+#pragma omp parallel for
+    for (int i = 0; i < n; i++)
+    {
+      data[i] = data_copy[i];
+    }
+    // padding
+#pragma omp parallel for
+    for (int i = n; i < n_dy; i++)
+    {
+      data[i] = 0;
+    }
+  }
+
+  gsl_wavelet *w;
+  gsl_wavelet_workspace *work;
+  w = gsl_wavelet_alloc(gsl_wavelet_daubechies, 4);
+  work = gsl_wavelet_workspace_alloc(n_dy);
+
+  double *p_data = data.data();
+  gsl_wavelet_transform_forward(w, p_data, 1, n_dy, work);
+
+  double max_abscoeff;
+  // auto comp = [](double a, double b) { return std::fabs(a) < std::fabs(b);
+  // }; max_abscoeff = std::fabs(*(max_element(data.begin(), data.end(),
+  // comp)));
+  max_abscoeff = std::fabs(data[0]);
+  for (int i = 1; i < data.size(); i++)
+  {
+    if (max_abscoeff < std::fabs(data[i]))
+    {
+      max_abscoeff = std::fabs(data[i]);
+    }
+  }
+
+  ids.clear();
+  coeffs.clear();
+  // int n_zeros = 0;
+  double abs_thre = (relative_threshold * max_abscoeff);
+  for (int i = 0; i < n_dy; i++)
+  {
+    if ((std::fabs(data[i]) > abs_thre) ||
+        (std::fabs(std::fabs(data[i]) - abs_thre) < 1e-15))
+    {
+      ids.push_back(i);
+      coeffs.push_back(data[i]);
+      //        data[i] = 0;
+      //        n_zeros++;
+    }
+  }
+  gsl_wavelet_free(w);
+  gsl_wavelet_workspace_free(work);
+  // printf("Max abscoeff=%f\n", max_abscoeff);
+  // printf("Number of padding elements: %d\n", n_dy - n);
+  // printf("number of zero coefficients=%d\n", n_zeros);
+  // printf("number of non-zero coefficients=%d\n", n_dy - n_zeros);
+  // printf("Compression ratio=%f\n", n * 1.0 / (1.0 * n_dy - 1.0 * n_zeros));
+  return n_dy;
+}
+
+int Fwd::wavelet_transform_vec(vector<double> &data) const
+{
+  int n = data.size();
+  int n_dy;
+
+  double log_value = std::log2(1.0 * n);
+  if (std::fabs(log_value - int(log_value)) < 1e-15)
+  {
+    n_dy = n;
+  }
+  else
+  {
+    n_dy = std::pow(2, ceil(log2(1.0 * n)));
+    vector<double> data_copy = data;
+    data.resize(n_dy);
+#pragma omp parallel for
+    for (int i = 0; i < n; i++)
+    {
+      data[i] = data_copy[i];
+    }
+    // padding
+#pragma omp parallel for
+    for (int i = n; i < n_dy; i++)
+    {
+      data[i] = 0;
+    }
+  }
+
+  gsl_wavelet *w;
+  gsl_wavelet_workspace *work;
+  w = gsl_wavelet_alloc(gsl_wavelet_daubechies, 4);
+  work = gsl_wavelet_workspace_alloc(n_dy);
+
+  double *p_data = data.data();
+  gsl_wavelet_transform_forward(w, p_data, 1, n_dy, work);
+
+  gsl_wavelet_free(w);
+  gsl_wavelet_workspace_free(work);
+  // printf("Max abscoeff=%f\n", max_abscoeff);
+  // printf("Number of padding elements: %d\n", n_dy - n);
+  // printf("number of zero coefficients=%d\n", n_zeros);
+  // printf("number of non-zero coefficients=%d\n", n_dy - n_zeros);
+  // printf("Compression ratio=%f\n", n * 1.0 / (1.0 * n_dy - 1.0 * n_zeros));
+  return n_dy;
+}
+
+void Fwd::set_use_wavelet(bool use_wavelet0)
+{
+  this->use_wavelet = use_wavelet0;
 }
--- src/Forward_modelling/Fwd.h
+++ src/Forward_modelling/Fwd.h
@@ -1,56 +1,73 @@
 #ifndef _FWD
 #define _FWD
+#include <gsl/gsl_sort.h>
+#include <gsl/gsl_wavelet.h>
+
 #include <bitset>
-#include"GravFormula.h"
-#include "gs.h"
-#include "Observation.h"
+
+#include "GravFormula.h"
 #include "Mesh.h"
+#include "Observation.h"
+#include "gs.h"
 using namespace std;
 
-class Fwd
-{
-public:
+class Fwd {
+ public:
   Fwd();
-  Fwd(const Mesh& mesh_, const Observation& ob_, unsigned long long field_flag_ = Compute_g_z);
-  Fwd(const Mesh& mesh_, const Observation& ob_, bitset<10> field_flag);
+  Fwd(const Mesh &mesh_, const Observation &ob_,
+      unsigned long long field_flag_ = Compute_g_z);
+  Fwd(const Mesh &mesh_, const Observation &ob_, bitset<10> field_flag);
   virtual ~Fwd();
 
-  VectorXd compute_gobs(const VectorXd& rho){
-    VectorXd d=this->G*rho;
+  VectorXd compute_gobs(const VectorXd &rho) {
+    VectorXd d = this->G * rho;
     return d;
   }
 
-  void set_field_flag(unsigned long long field_flag1){
-    field_flag=field_flag1;
+  void set_field_flag(unsigned long long field_flag1) {
+    field_flag = field_flag1;
   }
 
   void compute_G();
+  void compute_G_wavelet();
 
-  //bind mesh of tesseroids
-  void set_mesh(const Mesh& mesh0);
+  void set_use_wavelet(bool use_wavelet0);
+  // bind mesh of tesseroids
+  void set_mesh(const Mesh &mesh0);
 
-  //bind computation points
-  void set_observation(const Observation& ob0);
+  // bind computation points
+  void set_observation(const Observation &ob0);
 
   const Eigen::MatrixXd &get_G() { return G; }
 
-  unsigned int get_n_fields()
-  {
-    return field_flag.count();
-  }
+  unsigned int get_n_fields() { return field_flag.count(); }
 
-protected:
+ protected:
   Eigen::MatrixXd G;
   Mesh mesh;
   Observation ob;
-  int Nm;    //number of parameters
-  int N_obs; //number of data points
-  int Nd;    //number of observations
+  int Nm;     // number of parameters
+  int N_obs;  // number of data points
+  int Nd;     // number of observations
 
   bitset<10> field_flag;
 
-  void display_info_fields()const;
-  /** 
+  // wavelet support
+  bool use_wavelet;
+  int n_dy_for_wavelet;
+  // relative threshold for wavelet compression, default 0.005
+  double compression_threshold;
+
+  vector<vector<int>> comp_col_ids;
+  vector<vector<double>> comp_G_coeffs;
+
+  int inverse_wavelet_transform_vec(vector<double> &data) const;
+  int compress_vec(vector<double> &data, vector<int> &ids,
+                   vector<double> &coeffs, double relative_threshold);
+  int wavelet_transform_vec(vector<double> &data) const;
+
+  void display_info_fields() const;
+  /**
    * 0 V
    * 1 g_z
    * 2 g_x
