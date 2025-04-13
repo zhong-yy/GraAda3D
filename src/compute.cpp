ab94e6bca1aaf385a06aa11cf8ec724b4c364f62
Yiyuan Zhong <yiyuan.zhong@link.cuhk.edu.hk>
2025-04-13 15:09:01 +0800

    Utility executable for computing gravity

--- src/compute.cpp
+++ src/compute.cpp
@@ -37,10 +37,40 @@ std::vector<std::string> split_to_string(const std::string &s, char delim)
 
     while (getline(ss, item, delim))
     {
-        result.push_back(std::stod(item));
+        result.push_back(item);
     }
 
     return result;
+}
+
+void line_process(std::string &line, const std::string comment_str = "#")
+{
+    for (char &c : line) // C++11以上版本的语法
+    {
+        // 制表符 tab，逗号，分号都当作有效的分隔符，统一转成空格
+        // 为了避免错误，回车符和换行符也转为空格（否则无法处理空行）
+        if (c == '\t' || c == ',' || c == ';' || c == '\r' || c == '\n')
+            c = ' ';
+    }
+
+    // 查找注释符所在位置，如果不存在，则得到string::npos
+    int n_comment_start = line.find_first_of(comment_str);
+    if (n_comment_start != std::string::npos) // 这一句必须的
+        line.erase(n_comment_start);          // 删除注释
+
+    line.erase(0, line.find_first_not_of(" ")); // 删除行首空格
+    line.erase(line.find_last_not_of(" ") + 1); // 删除行末空格
+    // 调用的string& erase (size_t pos = 0, size_t len = npos);
+    //  len为默认参数,
+    //  size_t可以当作无符号整数，npos是string内置的静态常量，为size_t的最大值
+
+    /****************************************************************
+     *处理完毕。如果这一行只有空格，制表符 tab，注释，那么处理后line为空；
+     *如果行首有多个空格(或者空格和tab交错)，行尾为注释，如
+     *“   a b c#坐标”
+     *那么处理后字符串line的行首多个空格(和tab)和行尾注释被删掉，得到
+     *“a b c”
+     ****************************************************************/
 }
 
 int main(int ac, char *av[])
@@ -50,7 +80,7 @@ int main(int ac, char *av[])
         int opt;
         int portnum;
         po::options_description desc("Allowed options");
-        desc.add_options()("help,h", "produce help message")("model,m", po::value<string>(), "Specify the model file")("observation,p", po::value<string>(), "specify a file containing locations of observation points. Each line of this file should be three floating-point numbers corresponding to x y z coordinates of an observation point.")("component,c", po::value<string>(&field_str)->default_value("V/gz/gx/gy/Txx/Txy/Txz/Tyy/Tyz/Tzz"), "specify the components of gravitational potential/vector/gradient tensor to be components. The possible values are: \n\tV: gravitational potential\n\tgx/gy/gz: x/y/z component of gravity filed\n\tTxx/Txy/Txz/Tyy/Tyz/Tzz.\nFor example, the option '-c gx/gz/Txx/Tyz/Tzz' makes the program to calculate gx, gz, Txx, Tyz and Tzz.")("output,o", po::value<string>(), "output file");
+        desc.add_options()("help,h", "produce help message")("model,m", po::value<string>(), "Specify the model file")("observation,p", po::value<string>(), "specify a file containing locations of observation points. Each line of this file should be three floating-point numbers corresponding to x y z coordinates of an observation point.")("component,c", po::value<string>()->default_value("V/gz/gx/gy/Txx/Txy/Txz/Tyy/Tyz/Tzz"), "specify the components of gravitational potential/vector/gradient tensor to be components. The possible values are: \n\tV: gravitational potential\n\tgx/gy/gz: x/y/z component of gravity filed\n\tTxx/Txy/Txz/Tyy/Tyz/Tzz.\nFor example, the option '-c gx/gz/Txx/Tyz/Tzz' makes the program to calculate gx, gz, Txx, Tyz and Tzz.")("output,o", po::value<string>(), "output file");
 
         po::positional_options_description p;
         // p.add("input-file", -1);
@@ -122,11 +152,11 @@ int main(int ac, char *av[])
             }
         }
 
-        // unsigned long long field_flag;
-        bitset<10> field_flag;
+        unsigned long long field_flag_ULL;
+
         if (vm.count("component"))
         {
-            // string field_str = vm["component"].as<string>();
+            string field_str = vm["component"].as<string>();
             vector<string> component_strings = split_to_string(field_str, '/');
             cout << "The following field components will be computed: " << endl;
             for (int i = 0; i < component_strings.size(); i++)
@@ -136,56 +166,54 @@ int main(int ac, char *av[])
             cout << endl;
             for (int i = 0; i < component_strings.size(); i++)
             {
-                switch (component_strings[i])
+                if (component_strings[i] == "V")
                 {
-                case "V":
-                    field_flag = field_flag | Compute_V;
-                    break;
-                case "gz":
-                    field_flag = field_flag | Compute_g_z;
-                    break;
-                case "gx":
-                    field_flag = field_flag | Compute_g_x;
-                    break;
-                case "gy":
-                    field_flag = field_flag | Compute_g_y;
-                    break;
-                case "Txx":
-                    field_flag = field_flag | Compute_T_xx;
-                    break;
-                case "Txy":
-                    field_flag = field_flag | Compute_T_xy;
-                    break;
-                case "Tyx":
-                    field_flag = field_flag | Compute_T_xy;
-                    break;
-                case "Txz":
-                    field_flag = field_flag | Compute_T_zx;
-                    break;
-                case "Tzx":
-                    field_flag = field_flag | Compute_T_zx;
-                    break;
-                case "Tyy":
-                    field_flag = field_flag | Compute_T_yy;
-                    break;
-                case "Tyz":
-                    field_flag = field_flag | Compute_T_zy;
-                    break;
-                case "Tzy":
-                    field_flag = field_flag | Compute_T_zy;
-                    break;
-                case "Tzz":
-                    field_flag = field_flag | Compute_T_zz;
-                    break;
-                default:
+                    field_flag_ULL = field_flag_ULL | Compute_V;
+                }
+                else if (component_strings[i] == "gz")
+                {
+                    field_flag_ULL = field_flag_ULL | Compute_g_z;
+                }
+                else if (component_strings[i] == "gx")
+                {
+                    field_flag_ULL = field_flag_ULL | Compute_g_x;
+                }
+                else if (component_strings[i] == "gy")
+                {
+                    field_flag_ULL = field_flag_ULL | Compute_g_y;
+                }
+                else if (component_strings[i] == "Txx" || component_strings[i] == "gxx")
+                {
+                    field_flag_ULL = field_flag_ULL | Compute_T_xx;
+                }
+                else if (component_strings[i] == "Txy" || component_strings[i] == "Tyx" || component_strings[i] == "gxy" || component_strings[i] == "gyx")
+                {
+                    field_flag_ULL = field_flag_ULL | Compute_T_xy;
+                }
+                else if (component_strings[i] == "Txz" || component_strings[i] == "Tzx" || component_strings[i] == "gxz" || component_strings[i] == "gzx")
+                {
+                    field_flag_ULL = field_flag_ULL | Compute_T_zx;
+                }
+                else if (component_strings[i] == "Tyy" || component_strings[i] == "gyy")
+                {
+                    field_flag_ULL = field_flag_ULL | Compute_T_yy;
+                }
+                else if (component_strings[i] == "Tyz" || component_strings[i] == "Tzy" || component_strings[i] == "gyz" || component_strings[i] == "gzy")
+                {
+                    field_flag_ULL = field_flag_ULL | Compute_T_zy;
+                }
+                else if (component_strings[i] == "Tzz" || component_strings[i] == "gzz")
+                {
+                    field_flag_ULL = field_flag_ULL | Compute_T_zz;
+                }
+                else
+                {
                     cout << "Invalid component name" << endl;
                     return 1;
-                    break;
                 }
             }
         }
-        string outfile = vm["output"].as<string>();
-        ofstream out_s(outfile);
+        bitset<10> field_flag(field_flag_ULL);
 
         vector<unsigned int> field_index;
         for (unsigned int i = 0; i < 10; i++)
@@ -198,13 +226,13 @@ int main(int ac, char *av[])
         vector<vector<double>> results;
         results.resize(ob.get_n_obs());
         unsigned int n_components = field_index.size();
-        for (unsigned int ob_i = 0; ob_i < ob.get_n_obs(); ob_i++)
+        for (unsigned int i = 0; i < ob.get_n_obs(); i++)
         {
 
-            results[ob_i].resize(n_components);
-            for (unsigned int comp_i = 0; j < n_components; j++)
+            results[i].resize(n_components);
+            for (unsigned int k = 0; k < n_components; k++)
             {
-                results[ob_i][comp_i] == 0.0;
+                results[i][k] == 0.0;
             }
         }
 
@@ -224,6 +252,8 @@ int main(int ac, char *av[])
             }
         }
 
+        string outfile = vm["output"].as<string>();
+        ofstream out_s(outfile);
         vector<string> field_names_in_header = {"V (m2/s2)", "g_z (mGal)", "g_x (mGal)", "g_y (mGal)", "T_zz (E)",
                                                 "T_xz (E)", "T_yz (E)", "T_xx (E)", "T_xy (E)", "T_yy (E)"};
         out_s << setw(23) << left << "# x" << setw(23) << left << "y" << setw(23) << left << "z";
